58a6e461ffcf4b99872bdd12c2af1f435ae18fe8
AKSUMRUS <shimorhershow@gmail.com>
2022-07-03 12:44:27 +0600

    smth

--- app/src/main/java/com/ledokol/thebestprojectever/data/local/game/GameState.kt
+++ app/src/main/java/com/ledokol/thebestprojectever/data/local/game/GameState.kt
@@ -1,7 +1,9 @@
 package com.ledokol.thebestprojectever.data.local.game
 
+import androidx.lifecycle.LiveData
+
 data class GameState(
-    val games: List<Game>? = emptyList(),
+    val games: List<Game>? = mutableListOf(),
     val isLoading: Boolean = false,
     val isRefreshing: Boolean = false,
     val searchQuery: String = ""
